3ea896989d5658e4d8c06008bc809b1134e3b7d4
Apfelkuch <spiele55337@freenet.de>
2023-12-16 23:49:01 +0100

    Solution for Day 6 - Part 2

--- Day_6/app.js
+++ Day_6/app.js
@@ -9,48 +9,67 @@ fs.readFile(filenames[1], "utf8", (err, data) => {
   process(data);
 });
 
-const races = [];
+// const races = [];
+const race = {};
 
 function process(data) {
-  let content = [];
+  // let content = [];
+  // data.split("\n").forEach((element) => {
+  //   content.push(
+  //     element
+  //       .split(":")[1]
+  //       .split(" ")
+  //       .filter((elem) => elem)
+  //       .map((elem) => Number.parseInt(elem))
+  //   );
+  // });
+  // console.log(content);
+  // for (let i = 0; i < content[0].length; i++) {
+  //   let race = {};
+  //   race.time = content[0][i];
+  //   race.distance = content[1][i];
+  //   races.push(race);
+  // }
+  let lines = [];
   data.split("\n").forEach((element) => {
-    content.push(
-      element
-        .split(":")[1]
-        .split(" ")
-        .filter((elem) => elem)
-        .map((elem) => Number.parseInt(elem))
+    lines.push(
+      Number.parseInt(
+        element
+          .split(":")[1]
+          .split(" ")
+          .filter((elem) => elem)
+          .join("")
+      )
     );
   });
-  console.log(content);
-  for (let i = 0; i < content[0].length; i++) {
-    let race = {};
-    race.time = content[0][i];
-    race.distance = content[1][i];
-    races.push(race);
-  }
-  console.log(races);
+  // console.log(lines);
+  race.time = lines[0];
+  race.distance = lines[1];
+  // console.log(race);
+  // console.log(races);
 
-  let recordBeatingProduct = 1;
-  for (let race of races) {
-    let recordBeatingCount = 0;
-    let time = race.time;
-    let distance = race.distance;
-    for (let i = 1; i <= Math.floor(race.time / 2); i++) {
-      // console.log(i, time-i);
-      // console.log(distance);
-      // console.log((i * (time - i)) > distance);
-      if (i * (time - i) > distance) {
-        ++recordBeatingCount;
-      }
+  // let recordBeatingProduct = 1;
+  // for (let race of races) {
+  let recordBeatingCount = 0;
+  let time = race.time;
+  let distance = race.distance;
+  for (let i = 1; i <= Math.floor(race.time / 2); i++) {
+    // console.log(i, time-i);
+    // console.log(distance);
+    // console.log((i * (time - i)) > distance);
+    if (i * (time - i) > distance) {
+      ++recordBeatingCount;
     }
-    if (time % 2 == 0) {
-      recordBeatingCount -= 0.5;
-    }
-    // console.log("Count", recordBeatingCount);
-    recordBeatingCount *= 2;
-    recordBeatingProduct *= recordBeatingCount;
   }
-  console.log(recordBeatingProduct);
+  if (time % 2 == 0) {
+    recordBeatingCount -= 0.5;
+  }
+  // console.log("Count", recordBeatingCount);
+  recordBeatingCount *= 2;
+  // recordBeatingProduct *= recordBeatingCount;
+  // }
+  // console.log(recordBeatingProduct);
+  console.log(recordBeatingCount);
   // Part 1: 500346
+  // Part 2: 42515755
 }
